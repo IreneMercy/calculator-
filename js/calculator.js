a324834051f48c8555c96de32d813b9c2731d1c6
IreneMercy <irenemercy700@gmail.com>
2019-07-10 18:09:46 +0300

    add subtract, multiply and devide functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,11 +1,36 @@
-var add = function(number1, number2){
+// Add function//
+function add(number1, number2){
   return number1 + number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter a number:"));
-alert(add(number1, number2));
+var result = add(number1, number2);
+alert(result);
 
-var subtract = function(number1, number2){
+
+//Subtract function//
+function subtract(number1, number2){
   return number1-number2;
 };
-alert(subtract(10,5));
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"))
+var result = subtract(number1, number2);
+alert(result);
+
+// multiply function//
+function multiply(number1, number2){
+  return number1*number2;
+};
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"))
+var result = multiply(number1, number2);
+alert(result);
+
+// devide function//
+function devide(number1, number2){
+  return number1/number2;
+};
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"))
+var result = devide(number1, number2);
+alert(result);
